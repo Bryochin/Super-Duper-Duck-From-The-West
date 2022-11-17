b57d3dd1b96c5621c240ac5ab07042e4217a0a6c
Bryochin <bfremond@artfx.fr>
2022-11-17 16:19:06 +0100

    Update 17/11/22
    [feat]
    - Enemy Detecting & Shooting at Player.
    
    [wip]
    - Player Bullets Doing Damages to Enemies & disappearing.

--- Assets/Script/EnnemyShooting.cs
+++ Assets/Script/EnnemyShooting.cs
@@ -7,21 +7,30 @@ public class EnnemyShooting : MonoBehaviour
     public GameObject bullet;
     public Transform bulletPos;
     private float timer;
+    private GameObject player;
     // Start is called before the first frame update
     void Start()
     {
+        player = GameObject.FindGameObjectWithTag("Player");
     
     }
 
     // Update is called once per frame
     void Update()
     {
-        timer += Time.deltaTime;
 
-        if(timer > 2)
+        float distance = Vector2.Distance(transform.position, player.transform.position);
+        Debug.Log(distance);
+
+        if(distance < 5)
         {
-            timer = 0;
-            shoot();
+            timer += Time.deltaTime;
+
+            if (timer > 2)
+            {
+                timer = 0;
+                shoot();
+            }
         }
     }
 
